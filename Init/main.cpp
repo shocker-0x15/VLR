e1938e2ccb8a2474f6d0a02502392bb20a33b21f
shocker-0x15 <shocker.0x15@gmail.com>
2022-01-15 20:10:44 +0900

    add a new project to debug initialization issue.

---
+++ Init/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+#include <string>
+#include <sstream>
+#include <cuda.h>
+#include <vlr/vlrcpp.h>
+
+
+
+#define CUDADRV_CHECK(call) \
+    do { \
+        CUresult error = call; \
+        if (error != CUDA_SUCCESS) { \
+            std::stringstream ss; \
+            const char* errMsg = "failed to get an error message."; \
+            cuGetErrorString(error, &errMsg); \
+            ss << "CUDA call (" << #call << " ) failed with error: '" \
+               << errMsg \
+               << "' (" __FILE__ << ":" << __LINE__ << ")\n"; \
+            throw std::runtime_error(ss.str().c_str()); \
+        } \
+    } while (0)
+
+
+
+int32_t main(int32_t argc, const char* argv[]) {
+    CUcontext cuContext;
+    int32_t cuDeviceCount;
+    CUstream cuStream;
+    CUDADRV_CHECK(cuInit(0));
+    CUDADRV_CHECK(cuDeviceGetCount(&cuDeviceCount));
+    CUDADRV_CHECK(cuCtxCreate(&cuContext, 0, 0));
+    CUDADRV_CHECK(cuCtxSetCurrent(cuContext));
+    CUDADRV_CHECK(cuStreamCreate(&cuStream, 0));
+
+    vlr::ContextRef context = vlr::Context::create(cuContext, true);
+
+    return 0;
+}
